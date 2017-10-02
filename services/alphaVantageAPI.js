6698cd38c3b22db00fd14c83a7123080d62ff713
Cody Deckard <cjdeckard@gmail.com>
2017-10-02 20:56:56 +0000

    Basic Axios Install and API functionality
    Installs axios as a backend dependency and outlines a basic
    interface for calling the Alpha Vantage API.

---
+++ services/alphaVantageAPI.js
@@ -0,0 +1,25 @@
+'use strict'
+const axios = require('axios')
+
+
+const apiKey = process.env.avAPIKey || 'catsAreGreat'
+const uri = 'https://www.alphavantage.co/'
+
+const params = {
+    function: 'TIME_SERIES_DAILY',
+    apikey: apiKey,
+    
+}
+
+function getStockInfo(company) {
+    const specificParams = Object.assign({}, params) 
+    specificParams.symbol = company
+    axios.get(`${uri}/query`, {
+            params: specificParams
+        
+    }).then((resp) => console.log(resp)).catch((err) => console.warn(err))
+}
+
+module.exports = {
+    getStockInfo
+}
--- socketActions.js
+++ socketActions.js
@@ -1,4 +1,5 @@
 'use strict'
+const API = require('./services/alphaVantageAPI')
 
 module.exports  = function(io) {
     return { 
@@ -9,6 +10,7 @@ module.exports  = function(io) {
             client.on('new stock', (stock) => {
                 this.companies.push(stock)
                 io.emit('new stock', this.companies)    
+                API.getStockInfo('MSFT')
             })
     
             client.on('disconnect', ()=>console.log('A client disconnected'))
